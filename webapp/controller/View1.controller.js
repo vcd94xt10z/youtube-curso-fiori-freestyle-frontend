f33be1a8d3348bc8f5058cf855b501c4f0357895
Vinicius Cesar Dias <vcd94xt10z@users.noreply.github.com>
2024-04-28 17:15:34 -0300

    .

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -9,55 +9,16 @@ sap.ui.define([
 
         return Controller.extend("zov.controller.View1", {
             onInit: function () {
-                var oView  = this.getView();
-
-                var oModel1 = new sap.ui.model.json.JSONModel();
-                oModel1.setData({"usuario": {"nome": "José"}});
-                oModel1.setDefaultBindingMode(sap.ui.model.BindingMode.OneWay);
-                oView.setModel(oModel1,"oneway");
-
-                var oModel2 = new sap.ui.model.json.JSONModel();
-                oModel2.setData({"usuario": {"nome": "José"}});
-                oModel2.setDefaultBindingMode(sap.ui.model.BindingMode.TwoWay);
-                oView.setModel(oModel2,"twoway");
-
-                var oModel3 = new sap.ui.model.json.JSONModel();
-                oModel3.setData({"usuario": {"nome": "José"}});
-                oModel3.setDefaultBindingMode(sap.ui.model.BindingMode.OneTime);
-                oView.setModel(oModel3,"onetime");
             },
 
-            onTestOneWay: function(){
-                var oView  = this.getView();
-
-                var oModel = oView.getModel("oneway");
-                var oData  = oModel.getData();
-                oData.usuario.nome += ".";
-                oModel.setData(oData);
-                //oView.setModel(oModel,"oneway");
+            onNewCustomer: function(){
+                var r = sap.ui.core.UIComponent.getRouterFor(this);
+                r.navTo("RouteCustomerNew");
             },
 
-            onTestTwoWay: function(){
-                var oView  = this.getView();
-                
-                var oModel = oView.getModel("twoway");
-                var oData  = oModel.getData();
-                oData.usuario.nome += ".";
-                oModel.setData(oData);
-                //oView.setModel(oModel,"twoway");
-            },
-
-            onTestOneTime: function(){
-                var oView  = this.getView();
-                
-                var oModel = oView.getModel("onetime");
-                var oData  = oModel.getData();
-
-                debugger;
-
-                oData.usuario.nome += ".";
-                oModel.setData(oData);
-                //oView.setModel(oModel,"onetime");
+            onEditCustomer1: function(){
+                var r = sap.ui.core.UIComponent.getRouterFor(this);
+                r.navTo("RouteCustomerEdit",{CustomerId:1});
             }
         });
     });
