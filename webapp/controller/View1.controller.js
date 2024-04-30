5b7a7c09374c4ea5d3358d476d2b6d9ccc0f4b90
Vinicius Cesar Dias <vcd94xt10z@users.noreply.github.com>
2024-04-30 14:42:49 -0300

    Vídeo Function Import

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -12,7 +12,7 @@ sap.ui.define([
             onInit: function () {
                 var oView   = this.getView();
                 var oFModel = new sap.ui.model.json.JSONModel();
-    
+                
                 oFModel.setData({
                     "OrdemId": "",
                     "DataCriacao": "",
@@ -23,24 +23,71 @@ sap.ui.define([
                     "TotalOrdem": "",
                     "Status": "",
                     "OrdenacaoCampo": "OrdemId",
-                    "OrdenacaoTipo": "ASC"
+                    "OrdenacaoTipo": "ASC",
+                    "Limite": 10,
+                    "Offset": 0
                 });
                 oView.setModel(oFModel,"filter");
+
+                var oTModel = new sap.ui.model.json.JSONModel();
+                oTModel.setData([]);
+                oView.setModel(oTModel,"table");
     
                 this.onFilterSearch();
             },
 
+            onAtualizarStatus: function(sStatus){
+                var oTable   = this.getView().byId("table1");
+                var oModel   = this.getOwnerComponent().getModel();
+                var aIndex   = oTable.getSelectedIndices();
+                var that     = this;
+
+                if(aIndex.length == 0){
+                    MessageToast.show("Selecione uma linha");
+                    return;
+                }
+
+                if(aIndex.length != 1){
+                    MessageToast.show("Selecione apenas uma linha");
+                    return;
+                }
+
+                var oItem = oTable.getContextByIndex(aIndex[0]);
+                var iOrdemId = oItem.getProperty("OrdemId");
+
+                this.getView().setBusy(true);
+                oModel.callFunction(
+                    "/ZFI_ATUALIZA_STATUS",
+                    {
+                    method: "GET",
+                    urlParameters: {
+                        ID_ORDEMID: iOrdemId,
+                        ID_STATUS: sStatus
+                    },
+                    success: function(oData, response) {
+                        that.getView().setBusy(false);
+                        MessageToast.show("Status atualizado com sucesso");
+                        that.onFilterSearch();
+                    },
+                    error: function(oError) {
+                        that.getView().setBusy(false);
+                        MessageToast.show("Erro ao atualizar status");
+                    }
+                });
+            },
+
             onFilterReset: function(){
-    
             },
     
             onFilterSearch: function(oEvent){
                 var oView   = this.getView();
-                var oTable  = oView.byId("table1");
+                var oModel  = this.getOwnerComponent().getModel();
                 var oFModel = oView.getModel("filter");
+                var oTModel = oView.getModel("table");
                 var oFData  = oFModel.getData();
                 var oFilter = null;
                 var aParams = [];
+                var that    = this;
     
                 // aplicando filtros
                 var aSorter  = [];
@@ -79,12 +126,26 @@ sap.ui.define([
                 }
                 var oSort = new sap.ui.model.Sorter(oFData.OrdenacaoCampo,bDescending);
                 aSorter.push(oSort);
-    
+
+                // limite, offset
+                aParams.push("$top="+oFData.Limite);
+                aParams.push("$skip="+oFData.Offset);
+
                 // executando filtro
-                oTable.bindRows({
-                    path: '/OVCabSet',
-                    sorter: aSorter,
-                    filters: aFilters
+                this.getView().setBusy(true);
+                oModel.read("/OVCabSet",{
+                    sorters: aSorter,
+                    filters: aFilters,
+                    urlParameters: aParams,
+
+                    success: function(oData2, oResponse){
+                        that.getView().setBusy(false);
+                        oTModel.setData(oData2.results);
+                    },
+                    error: function(oError){
+                        that.getView().setBusy(false);
+                        MessageToast.show("Erro");
+                    }
                 });
             }
         });
