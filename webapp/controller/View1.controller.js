d4900e822124e034e1c6a68294a802d825b12b80
Vinicius Cesar Dias <vcd94xt10z@users.noreply.github.com>
2024-05-13 20:59:27 -0300

    Vídeo Dialog

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -12,8 +12,30 @@ sap.ui.define([
             onInit: function () {
             },
 
-            onHeader: function(){
-                alert("onHeader");
+            onOpenDialogInfo() {
+                var that  = this;
+                var sName = "zov.view.DialogInfo";
+                
+                if(!this.oDialog){
+                    /*
+                    this.loadFragment({
+                        name: sName
+                    }).then(function(oDialog2) {
+                        that.oDialog = oDialog2;
+                        that.oDialog.open();
+                    }.bind(this));
+                    */
+
+                    this.oDialog = this.byId("DialogInfo");
+                    that.oDialog.open();
+                }else{
+                    this.oDialog.open();
+                }
+            },
+            
+            onCloseDialogInfo: function(){
+                //this.byId("DialogInfo").close();
+                this.oDialog.close();
             }
         });
     });
