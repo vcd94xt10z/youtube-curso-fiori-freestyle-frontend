5022088690ebc4a263cb20cf8af3bb12247aec09
Vinicius Cesar Dias <vcd94xt10z@users.noreply.github.com>
2024-06-06 17:26:46 -0300

    Vídeo Atualização de Status Ordem Function Import

--- webapp/controller/OrdemList.controller.js
+++ webapp/controller/OrdemList.controller.js
@@ -13,6 +13,10 @@ sap.ui.define([
         return Controller.extend("zov.controller.OrdemList", {
             formatter: formatter,
 
+            oDialogMessageList: null,
+            aUpdateStatusQueue: [],    // array da fila de atualização de status
+            aUpdateStatusMessages: [], // array da mensagens
+
             onInit: function(){
                 var oView   = this.getView();
                 var oFModel = new sap.ui.model.json.JSONModel();
@@ -137,6 +141,113 @@ sap.ui.define([
             
             _onRouteMatchedList: function(oEvent){
                 this.onFilterSearch();
+            },
+
+            onChangeStatus: function(sStatus){
+                var that   = this;
+                var oView = that.getView();
+                var oTable = oView.byId("table1");
+                var aData  = oView.getModel("table").getData();
+
+                var aIndex = oTable.getSelectedIndices();
+                if(aIndex.length == 0){
+                    MessageToast.show("Marque ao menos 1 item");
+                    return;
+                }
+
+                this.aUpdateStatusQueue    = [];
+                this.aUpdateStatusMessages = [];
+
+                for(var i in aIndex){
+                    try {
+                        var iIndex   = aIndex[i];
+                        //var oRow     = oTable.getRows()[iIndex];
+                        //var sOrdemId = oRow.getCells()[0].getText();
+                        var sOrdemId = aData[iIndex].OrdemId;
+
+                        this.aUpdateStatusQueue.push({
+                            OrdemId: sOrdemId,
+                            Status: sStatus
+                        });
+                    }catch(e){
+                        console.log(e);
+                        console.log("i="+i);
+                        console.log("iIndex="+iIndex);
+                    }
+                }
+
+                this.runUpdateStatusQueue();
+            },
+
+            runUpdateStatusQueue: function(){
+                var that   = this;
+                var oQueue = this.aUpdateStatusQueue.pop();
+
+                if(oQueue == undefined){
+                    this.getView().setBusy(false);
+                    this.onOpenMessageListDialog(this.aUpdateStatusMessages);
+                    this.onFilterSearch();
+                    return;
+                }
+
+                var oModel = this.getOwnerComponent().getModel();
+                this.getView().setBusy(true);
+                oModel.callFunction(
+                    "/ZFI_ATUALIZA_STATUS",
+                    {
+                    method: "GET",
+                    urlParameters: {
+                        ID_ORDEMID: oQueue.OrdemId,
+                        ID_STATUS: oQueue.Status
+                    },
+                    success: function(oData, response) {
+                        for(var i in oData.results){
+                            that.aUpdateStatusMessages.push(oData.results[i]);
+                        }
+                        that.runUpdateStatusQueue();
+                    },
+                    error: function(oResponse) {
+                        try {
+                            var oError = JSON.parse(oResponse.responseText);
+                            //MessageToast.show(oError.error.message.value);
+                            that.aUpdateStatusMessages.push({
+                                "Tipo": "E",
+                                "Mensagem": "Erro ao atualizar ordem "+oQueue.OrdemId+": "+oError.error.message.value 
+                            });
+                        }catch(e){
+                            that.aUpdateStatusMessages.push({
+                                "Tipo": "E",
+                                "Mensagem": "Erro ao atualizar ordem "+oQueue.OrdemId 
+                            });
+                        }
+
+                        that.runUpdateStatusQueue();
+                    }
+                });
+            },
+
+            onOpenMessageListDialog: function(aMessageList){
+                var that  = this;
+                var sName = "zov.view.MessageList";
+
+                var oModel = new sap.ui.model.json.JSONModel(aMessageList);
+                this.getView().setModel(oModel,"messageList");
+                
+                if(!this.oDialogMessageList){
+                    this.loadFragment({
+                        name: sName
+                    }).then(function(oDialog2) {
+                        that.oDialogMessageList = oDialog2;
+
+                        that.oDialogMessageList.open();
+                    }.bind(this));
+                }else{
+                    this.oDialogMessageList.open();
+                }
+            },
+
+            onCloseMessageListDialog: function(){
+                this.byId("MessageListDialog").close();
             }
         });
     });
--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -20,6 +20,32 @@ sap.ui.define([
             }
             const oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
 			return oResourceBundle.getText("Status"+sValue);
+        },
+
+        getIconStatus: function(sStatus){
+            var sIcon = "sap-icon://message-information";
+            switch(sStatus){
+            case "S":
+                sIcon = "sap-icon://message-success";
+                break;
+            case "E":
+                sIcon = "sap-icon://message-error";
+                break;
+            }
+            return sIcon;
+        },
+
+        getColorStatus: function(sStatus){
+            var sColor = "#FFFFFF";
+            switch(sStatus){
+            case "S":
+                sColor = "#00AA00";
+                break;
+            case "E":
+                sColor = "#AA0000";
+                break;
+            }
+            return sColor;
         }
     };
 });
