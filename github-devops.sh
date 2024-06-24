7aa2da02f6293b5b9a65bce7df2ac3cf62d17af8
Vinicius Cesar Dias <vcd94xt10z@users.noreply.github.com>
2024-06-24 12:42:53 -0300

    .

--- github-devops.sh
+++ github-devops.sh
@@ -1,22 +1,39 @@
 npm install
 
 # instala as dependências
-echo Dependencias
+echo -------------------------------------------------------------------
+echo [01/06] Dependencias
+echo -------------------------------------------------------------------
 npm install --save-dev @ui5/cli
 npm install --save-dev nwabap-ui5uploader
 npm install --save-dev ui5-test-runner
 
 # antes de executar, iniciar servidor com dados mock (no linux, coloque um & no final para rodar em background)
-ui5 serve --config ui5-mock.yaml --port 8080 &
+echo -------------------------------------------------------------------
+echo [02/06] Subindo servidor Web em background para executar os testes
+echo -------------------------------------------------------------------
+ui5 serve --config ui5-mock.yaml --port 8085 &
 
 # testes unitários
-npx ui5-test-runner --url http://localhost:8080/test/unit/unitTests.qunit.html
+echo -------------------------------------------------------------------
+echo [03/06] Executando testes unitários
+echo -------------------------------------------------------------------
+npx ui5-test-runner --url http://localhost:8085/test/unit/unitTests.qunit.html
 
 # testes integrados
-npx ui5-test-runner --url http://localhost:8080/test/integration/opaTests.qunit.html
+echo -------------------------------------------------------------------
+echo [04/06] Executando testes integrados
+echo -------------------------------------------------------------------
+npx ui5-test-runner --url http://localhost:8085/test/integration/opaTests.qunit.html
 
 # executar build
+echo -------------------------------------------------------------------
+echo [05/06] Executando build
+echo -------------------------------------------------------------------
 ui5 build
 
 # deploy
+echo -------------------------------------------------------------------
+echo [06/06] Executando deploy
+echo -------------------------------------------------------------------
 npx nwabap upload
